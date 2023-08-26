e6747a9ac31e9d3f0c6cab499040e3533d5c3686
Gabriel Silva dos Santos <Gabrielsilva996@gmail.com>
2023-08-26 17:42:07 -0300

    Refatorando footer com MUI

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,30 +1,22 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 import GitHub from "../../assets/github.png";
 import In from "../../assets/in.png";
 
-import {
-    FooterContainer,
-    TextTopicFooter,
-    LinkSocialMedias,
-} from './styles'
-
 export default function Footer () {
 
     return(
-        <FooterContainer>
-            <TextTopicFooter>
-                © - Gabriel Silva dos Santos
-            </TextTopicFooter>
-            
-            <TextTopicFooter>
-                <LinkSocialMedias target="_blank" href="https://github.com/Sorriso337">
-                    <img src={GitHub} />
-                </LinkSocialMedias>
-                <LinkSocialMedias target="_blank" href="https://www.linkedin.com/in/gabriel-silva-8a7461198">
-                    <img src={In} />
-                </LinkSocialMedias>
-            </TextTopicFooter>
-        </FooterContainer>
+        <Grid position='fixed' bottom={0} container padding={1} display='flex' justifyContent='space-between'  style={{backgroundColor: '#24292F'}}>
+            <Grid item>
+                <Typography color='white' variant='h6' fontWeight='bold'>
+                    © - Gabriel Silva dos Santos
+                </Typography>
+            </Grid>
+            <Grid item>
+                <img style={{ cursor: 'pointer', margin: '0 4px' }} onClick={ () => window.open("https://github.com/Sorriso337", "_blank" ) } alt='' src={GitHub} />
+                <img style={{ cursor: 'pointer', margin: '0 4px' }} onClick={ () => window.open("https://www.linkedin.com/in/gabriel-silva-8a7461198", "_blank" ) } alt='' src={In} />
+            </Grid>
+        </Grid>
 
     )
 }
