66506c481c8fc09fbbc5aedf07f7a7b6729d5e54
Gabriel Silva dos Santos <Gabrielsilva996@gmail.com>
2023-08-26 15:57:10 -0300

    Refatorando Header com MUI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import RenderItemCard from './components/RenderItemCard';
 import GlobalStyles from './styles/GlobalStyles';
+import { Grid } from '@mui/material';
 
 const App = () => {
   return (
-    <div id='root'>
+    <Grid container id='root'>
       <GlobalStyles/>
         <Header/>
         <RenderItemCard />
         <Footer />
-    </div>
+    </Grid>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,21 @@
 import React, { useContext } from 'react';
-import { 
-    HeaderContainer, 
-    SearchInput, 
-    SearchButton,
-    TitleMessage,
-    SearchContainer, 
-} from "./styles"
-
 import GlobalContext from "../../contexts/globalContext";
+import { Button, Grid, TextField, Typography } from '@mui/material';
 
 export default function Header () {
     const { text, textModifier, makeRequest } = useContext(GlobalContext)
 
     return(
-        <HeaderContainer>
-            <TitleMessage>Busque um repositório do GitHub 🧐​</TitleMessage>
-            <SearchContainer>
-                <SearchInput placeholder="Digite aqui o Repositório que busca" defaultValue={'Node'} onChange={(event) => textModifier(event.target.value)}/>
-                <SearchButton disabled={text == ''} onClick={() => makeRequest()}>Buscar </SearchButton>
-            </SearchContainer>
-        </HeaderContainer>
-
+        <Grid margin={1} display='flex' justifyContent='center' alignItems='center' container spacing={6}>
+            <Grid item md={12}>
+                <Typography color='white' textAlign='center' variant='h4' fontWeight='bold' >Busque um repositório do GitHub 🧐​</Typography>
+            </Grid>
+            <Grid item xs={10} md={9}>
+                <TextField onChange={e => textModifier(e.target.value)} fullWidth variant='outlined' label="Digite aqui o Repositório que busca" defaultValue={text}/>
+            </Grid>
+            <Grid  item md={2}>
+                <Button size='large' variant='contained' disabled={text === ''} onClick={() => makeRequest()}>Buscar </Button>
+            </Grid>
+        </Grid>
     )
 }
